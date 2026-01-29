7700f3fedb40dbf0e7fbecf738a0533727a36e9e
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-01-29 17:09:22 +0900

    app/createTable.hs

--- noskellay-stopgap/app/createTable.hs
+++ noskellay-stopgap/app/createTable.hs
@@ -1,4 +1,15 @@
 module Main where
 
+import Database.SmplstSQLite3
+
 main :: IO ()
-main = putStrLn "CREATE TABLE"
+main = do
+	putStrLn "CREATE TABLE"
+	withSQLite "foo.sqlite3" $ \db -> do
+		withPrepared db (
+			"CREATE TABLE events(" ++
+			"id, pubkey, created_at, kind, " ++
+			(('"' :) . (: "\", ") =<< ['a' .. 'z']) ++
+			(("\"L" ++) . (: "\", ") =<< ['A' .. 'Z']) ++
+			"tags, content, sig)" ) step
+	pure ()
