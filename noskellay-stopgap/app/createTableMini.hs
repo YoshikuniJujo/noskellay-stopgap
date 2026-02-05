a975bace188c40d0ee458df1f9f1221356950115
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-02-05 17:22:29 +0900

    app/createTableMini.hs

---
+++ noskellay-stopgap/app/createTableMini.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import Database.SmplstSQLite3
+
+main :: IO ()
+main = do
+	putStrLn "CREATE TABLE"
+	withSQLite "foo_mini.sqlite3" $ \db -> do
+		withPrepared db (
+			"CREATE TABLE events(" ++
+			"id, pubkey, created_at, kind, " ++
+			(('"' :) . (: "\", ") =<< ['a' .. 'c']) ++
+			"tags, content, sig)" ) step
+	pure ()
