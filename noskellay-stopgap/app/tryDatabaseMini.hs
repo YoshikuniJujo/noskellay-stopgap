8e506149944ee0e37478cf172832260cab664ed8
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-02-06 15:33:25 +0900

    app/tryDatabaseMini.hs: Mini: insert and select

--- noskellay-stopgap/app/tryDatabaseMini.hs
+++ noskellay-stopgap/app/tryDatabaseMini.hs
@@ -6,9 +6,10 @@
 module Main (main) where
 
 import Database.SmplstSQLite3
-import Event.Mini.Database
+import Nostr.Event.Signed qualified as Signed
 import System.Environment
 
+import Event.Mini.Database
 import Samples
 
 main :: IO ()
@@ -19,48 +20,49 @@ main = do
 	print ev
 	let	ev' = fromSigned ev
 	print ev'
-	_ <- withSQLite "foo_mini.sqlite3" $ \db -> do
-		_ <- withPrepared db
-			"INSERT INTO events VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)" \sm -> do
-			bindN sm 1 $ idnt ev'
-			bindN sm 2 $ pubkey ev'
-			bindN sm 3 $ created_at ev'
-			bindN sm 4 $ kind ev'
-			bindN sm 5 $ a ev'
-			bindN sm 6 $ b ev'
-			bindN sm 7 $ c ev'
-			bindN sm 8 $ tags ev'
-			bindN sm 9 $ content ev'
-			bindN sm 10 $ sig ev'
-			step sm
+	withSQLite "foo_mini.sqlite3" $ \db -> do
+		print =<< insert db ev'
 		putStrLn ""
-		withPrepared db
-			"SELECT * FROM events" \sm -> do
-			_ <- step sm
-			(idnt' :: String) <- column sm 0
-			(pk :: String) <- column sm 1
-			(crat :: Int) <- column sm 2
-			(knd :: Int) <- column sm 3
-			(a' :: Maybe String) <- column sm 4
-			(b' :: Maybe String) <- column sm 5
-			(c' :: Maybe String) <- column sm 6
-			tgs <- column sm 7
-			{-
-			tgs <- (EvJsn.decodeTags <$>)
-				. A.decode . LBS.fromStrict <$> column sm 7
-				-}
-			(cnt :: String) <- column sm 8
-			(sg :: String) <- column sm 9
-			let e = E {
-				idnt = idnt',
-				pubkey = pk,
-				created_at = crat,
-				kind = knd,
-				a = a',
-				b = b',
-				c = c',
-				tags = tgs,
-				content = cnt,
-				sig = sg }
-			print $ toSigned e
-	pure ()
+		print =<< select db
+
+insert :: SQLite -> E -> IO (Result, String)
+insert db ev' = withPrepared db
+	"INSERT INTO events VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)" \sm -> do
+	bindN sm 1 $ idnt ev'
+	bindN sm 2 $ pubkey ev'
+	bindN sm 3 $ created_at ev'
+	bindN sm 4 $ kind ev'
+	bindN sm 5 $ a ev'
+	bindN sm 6 $ b ev'
+	bindN sm 7 $ c ev'
+	bindN sm 8 $ tags ev'
+	bindN sm 9 $ content ev'
+	bindN sm 10 $ sig ev'
+	step sm
+
+select :: SQLite -> IO (Signed.E, String)
+select db = withPrepared db
+	"SELECT * FROM events" \sm -> do
+	_ <- step sm
+	(idnt' :: String) <- column sm 0
+	(pk :: String) <- column sm 1
+	(crat :: Int) <- column sm 2
+	(knd :: Int) <- column sm 3
+	(a' :: Maybe String) <- column sm 4
+	(b' :: Maybe String) <- column sm 5
+	(c' :: Maybe String) <- column sm 6
+	tgs <- column sm 7
+	(cnt :: String) <- column sm 8
+	(sg :: String) <- column sm 9
+	let e = E {
+		idnt = idnt',
+		pubkey = pk,
+		created_at = crat,
+		kind = knd,
+		a = a',
+		b = b',
+		c = c',
+		tags = tgs,
+		content = cnt,
+		sig = sg }
+	pure $ toSigned e
