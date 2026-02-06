4feabfac9837c261b6f8ad2df9eafc62301f08c8
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-02-06 16:28:19 +0900

    move insert and selectAll1 to Event.Mini.Database

--- noskellay-stopgap/src/Event/Mini/Database.hs
+++ noskellay-stopgap/src/Event/Mini/Database.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Event.Mini.Database (
-	E(..), fromSigned, toSigned ) where
+	E(..), fromSigned, toSigned,
+	insert, selectAll1
+	) where
 
 import Data.Maybe
 import Data.ByteString qualified as BS
@@ -15,6 +19,8 @@ import Crypto.Curve.Secp256k1
 
 import Event.Database.Tools
 import Tools
+
+import Database.SmplstSQLite3
 
 data E = E {
 	idnt :: String,
@@ -58,3 +64,48 @@ toSigned e = Signed.E {
 -- TRY IT
 -- ghci> ev <- getSampleSigned "/path/to/sec_file" "/path/to/pub_file"
 -- ghci> Event.Mini.Database.fromSigned ev
+
+insert :: SQLite -> E -> IO (Result, String)
+insert db ev' = withPrepared db
+	"INSERT INTO events VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)" \sm -> do
+	bindN sm 1 $ idnt ev'
+	bindN sm 2 $ pubkey ev'
+	bindN sm 3 $ created_at ev'
+	bindN sm 4 $ kind ev'
+	bindN sm 5 $ a ev'
+	bindN sm 6 $ b ev'
+	bindN sm 7 $ c ev'
+	bindN sm 8 $ tags ev'
+	bindN sm 9 $ content ev'
+	bindN sm 10 $ sig ev'
+	bindN sm 11 $ verified ev'
+	step sm
+
+selectAll1 :: SQLite -> IO ((Result, Signed.E), String)
+selectAll1 db = withPrepared db
+	"SELECT * FROM events" \sm -> do
+	r <- step sm
+	(idnt' :: String) <- column sm 0
+	(pk :: String) <- column sm 1
+	(crat :: Int) <- column sm 2
+	(knd :: Int) <- column sm 3
+	(a' :: Maybe String) <- column sm 4
+	(b' :: Maybe String) <- column sm 5
+	(c' :: Maybe String) <- column sm 6
+	tgs <- column sm 7
+	(cnt :: String) <- column sm 8
+	(sg :: String) <- column sm 9
+	(vrf :: Bool) <- column sm 10
+	let e = E {
+		idnt = idnt',
+		pubkey = pk,
+		created_at = crat,
+		kind = knd,
+		a = a',
+		b = b',
+		c = c',
+		tags = tgs,
+		content = cnt,
+		sig = sg,
+		verified = vrf }
+	pure (r, toSigned e)
