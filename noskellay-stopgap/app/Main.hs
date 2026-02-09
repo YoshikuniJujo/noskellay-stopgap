fcb5735065ca5b06058b342f81d3cb9682d22a21
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-02-09 15:24:44 +0900

    app/Main.hs: insert and select

--- noskellay-stopgap/app/Main.hs
+++ noskellay-stopgap/app/Main.hs
@@ -7,32 +7,49 @@
 
 module Main (main) where
 
+import Control.Arrow
 import Data.Function
 import Data.Foldable
 import Data.Text qualified as T
 import Data.Aeson
 import Data.Aeson.KeyMap qualified as KM
 import Network.WebSockets
+import Database.SmplstSQLite3 qualified as SQL
+
+import Nostr.Event.Json qualified as EvJsn
+import Event.Database qualified as Db
 
 main :: IO ()
-main = runServer "0.0.0.0" 10000 \pconn -> acceptRequest pconn >>= \conn -> do
+main = SQL.withSQLite "foo.sqlite3" realMain
+
+realMain :: SQL.SQLite -> IO ()
+realMain db = runServer "0.0.0.0" 10000 \pconn -> acceptRequest pconn >>= \conn -> do
 	fix \go -> receive conn >>= \case
 		r@(DataMessage _ _ _ (Text rjsn _)) -> do
 			print r
-			(>> go) case recToSend =<< decode rjsn of
+			s <- maybe (pure Nothing) (recToSend db) $ decode rjsn
+			(>> go) case s of
 				Nothing -> pure ()
-				Just (encode -> sjsn) ->
-					sendDataMessage conn $ Text sjsn Nothing
+				Just ((encode <$>) -> sjsns) ->
+					(\snd -> sendDataMessage conn $ Text snd Nothing) `mapM_` sjsns
 		r@(ControlMessage (Close _ _)) -> print r
 		r -> print r >> go
 	sendClose conn ("Good-bye!" :: T.Text)
 
-recToSend :: Value -> Maybe Value
-recToSend = \case
-	Array (toList ->
-		String "EVENT" :
-			Object (KM.lookup "id" -> Just (String i)) : _) ->
-		Just $ Array [String "OK", String i, Bool True, String ""]
-	Array (toList -> String "REQ" : String i : _) ->
-		Just $ Array [String "EOSE", String i]
-	_ -> Nothing
+recToSend :: SQL.SQLite -> Value -> IO (Maybe [Value])
+recToSend db = \case
+	Array (toList -> String "EVENT" :
+			Object ((id &&& KM.lookup "id") -> (ev, Just (String i))) : _) -> do
+		Just ev' <- pure $ Db.fromSigned <$> EvJsn.decode' ev
+		_ <- Db.insert db ev'
+		print ev'
+		pure $ Just [
+			Array [String "OK", String i, Bool True, String ""]
+			]
+	Array (toList -> String "REQ" : String i : _) -> do
+		((_, ev), _) <- Db.selectAll1 db
+		pure . Just $ maybe id (\e ->
+				(Array [String "EVENT", String i, Object e] :))
+				(EvJsn.encode' ev)
+			[Array [String "EOSE", String i]]
+	_ -> pure Nothing
