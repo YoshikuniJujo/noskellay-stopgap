1cbb36fd738b859fe70b767416ef60049947c29b
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-02-17 10:29:25 +0900

    Event.NG.Mini.Database: fromSigned

--- noskellay-stopgap/src/Event/NG/Mini/Database.hs
+++ noskellay-stopgap/src/Event/NG/Mini/Database.hs
@@ -6,17 +6,41 @@ module Event.NG.Mini.Database where
 
 import Control.Monad
 import Data.Map qualified as Map
+import Data.ByteString qualified as BS
+import Data.ByteString.Lazy.Char8 qualified as LBSC
 import Data.Text qualified as T
+import Data.Aeson qualified as A
 import Data.UUIDv7
+import Crypto.Curve.Secp256k1
 import "try-nostr-event-ng" Nostr.Event.Signed qualified as Signed
+import "try-nostr-event-ng" Nostr.Event.Json qualified as EvJsn
 import Event.NG.Mini.Database.Type
 
+import Event.Database.Tools
+import Tools
+
 fromSigned :: Signed.E -> IO (E, [Tag])
-fromSigned e = undefined
+fromSigned e = do
+	dct <- genId
+	pure (fromSignedE dct e, tagsToTags dct $ Signed.tags e)
 
 genId :: IO (Map.Map T.Text UUIDv7)
 genId = Map.fromList
 	<$> zipWith ((,) . (T.:< "")) "abc" <$> replicateM 3 nextUUIDv7
+
+fromSignedE :: Map.Map T.Text UUIDv7 -> Signed.E -> E
+fromSignedE dct e = E {
+	idnt = bsToHexStr $ Signed.idnt e,
+	pubkey = bsToHexStr . BS.tail . serialize_point $ Signed.pubkey e,
+	created_at = unixTimeToInt $ Signed.created_at e,
+	kind = Signed.kind e,
+	a = dct Map.! "a",
+	b = dct Map.! "b",
+	c = dct Map.! "c",
+	tags = LBSC.unpack . A.encode . EvJsn.encodeTags $ Signed.tags e,
+	content = T.unpack $ Signed.content e,
+	sig = bsToHexStr $ Signed.sig e,
+	verified = Signed.verified e }
 
 tagsToTags :: Map.Map T.Text UUIDv7 -> [(T.Text, (T.Text, [T.Text]))] -> [Tag]
 tagsToTags dct = \case
